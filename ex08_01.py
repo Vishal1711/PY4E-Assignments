5ffc79ed70750716b9ebbdb088aaa4a3d9054bbd
Vishal Jadhav <79803663+Vishal1711@users.noreply.github.com>
2021-06-08 00:45:47 +0530

    Add files via upload

---
+++ ex08_01.py
@@ -0,0 +1,16 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    words = line.rstrip().split()
+    for element in words:
+
+
+        if element in lst:
+            continue
+
+        else:
+            lst.append(element)
+
+lst.sort()
+print(lst)
